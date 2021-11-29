634e457e9d7a204eeecd06101560fc600f7ed901
Stanley Eze <stanley.ezeifeanyi@gmail.com>
2021-11-29 22:21:35 +0100

    display list product

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -10,7 +10,7 @@ export class ProductService {
 
   private _albumUrl: string = '../assets/album.json';
 
-  private _productsUrl: string = '../products.json'
+  private _productsUrl: string = '../assets/products.json'
 
   constructor(private _http: Http) { }
 
